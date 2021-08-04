3da391260a463f8353d4e2b842c7eb3bb5ca2a45
Joe <joeabrah@cisco.com>
2021-08-04 14:10:43 -0700

    Modified Tip Calc with comments and rounding

--- day_002_tip_calc.py
+++ day_002_tip_calc.py
@@ -1,11 +1,18 @@
+#Prints the welcome message
 print("Welcome to the tip calculator")
 
-bill = input("What was the total bill?")
-tip_percent = input("What percentage tip would you like to give? 10, 12, 15, or 20?")
-num_people = input("How many people are there to split the check with?")
+#Add the inputs with defined data types for each
+bill = float(input("What was the total bill?"))
+tip_percent = int(input("What percentage tip would you like to give?"))
+num_people = int(input("How many people are there to split the check with?"))
 
-tip_amount = int(tip_percent)/100 * int(bill)
-total_bill = int(bill) + tip_amount
-bill_per_person = total_bill / int(num_people)
+#Convert the inputs to usable data
+tip_amount = (tip_percent/100) * bill
+total_bill = bill + tip_amount
 
-print(f"Each person should pay {bill_per_person}")
+#Figures the bill per person, rounding to the nearest 2 decimal points, and fixes decimal formatting with trailing zeros
+bill_per_person = round((total_bill / num_people), 2)
+bill_per_person = "{:.2f}".format(bill_per_person)
+
+#Print the results using a f-string (function is inside of stringusing {})
+print(f"Each person should pay ${bill_per_person}")
