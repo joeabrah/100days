5c47defa50597186f805e78f44ada4921f92eb30
Joe <joeabrah@cisco.com>
2021-08-04 13:53:57 -0700

    Changed band name gen, updated README

--- day_001_bandnamegen.py
+++ day_001_bandnamegen.py
@@ -1,8 +1,11 @@
 # This python script is to be used as a band name generator based on basic named input
 
+#Print the welcome text
 print("Welcome to the band name generator!")
 
+#Take inputs for the city name and pet name
 city_name = input("What city did you grow up in?\n")
 pet_name = input("What is your favorite pet's name?\n")
 
+#Combine the inputs as strings, and print the band name
 print("Your band name should be: " + city_name + " " + pet_name)
